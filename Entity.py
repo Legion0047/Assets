2d726b1ec447d4131418649188178a164320874b
Legion0047 <57983464+Legion0047@users.noreply.github.com>
2020-06-27 18:36:02 +0200

    fixed add

--- Entity.py
+++ Entity.py
@@ -1,3 +1,4 @@
+from functools import partial
 from random import randint
 
 from tkinter import *
@@ -5,7 +6,10 @@ from tkinter.ttk import *
 
 class entity:
     # Name, +x to any roll in sub entities/this entity, +10*x% to budget in any sub entities/this entity, +x to the budget, sub entity
-    def __init__(self, window, name, dev, bonus, mult, raw, entities=None):
+    def __init__(self, window, name="New Entity", dev=0, bonus=0, mult=0, raw=0, entities=None):
+        if entities is None:
+            entities = []
+        self.window = window
         self.name = name
         self.dev = dev
         self.bonus = bonus
@@ -13,72 +17,114 @@ class entity:
         self.raw = raw
         self.entities = entities
 
-        self.name_e = Entry(window, width=15)
+        # The entries for the entities stats
 
-        self.dev_e = Entry(window, width=15)
+        self.name_e = Entry(self.window, width=15)
+        self.name_e.delete(0, END)
+        self.name_e.insert(0, self.name)
 
-        self.bonus_e = Entry(window, width=15)
+        self.dev_e = Entry(self.window, width=15)
+        self.dev_e.delete(0, END)
+        self.dev_e.insert(0, self.dev)
 
-        self.mult_e = Entry(window, width=15)
+        self.bonus_e = Entry(self.window, width=15)
+        self.bonus_e.delete(0, END)
+        self.bonus_e.insert(0, self.bonus)
 
-        self.raw_e = Entry(window, width=15)
+        self.mult_e = Entry(self.window, width=15)
+        self.mult_e.delete(0, END)
+        self.mult_e.insert(0, self.mult)
+
+        self.raw_e = Entry(self.window, width=15)
+        self.raw_e.delete(0, END)
+        self.raw_e.insert(0, self.raw)
+
+        # The button to create a new entity
+
+        self.add_btn = Button(self.window, width=15, text="Add Entity", command=self.add)
+
+        # A button to remove entities
+
+        self.remove_btn = []
+
+        for i in range(len(self.entities)):
+            self.remove_btn.append(Button(self.window, width=15, text="Remove Entity", command=partial(self.remove, i)))
 
     def calcBudget(self, bonus=0):
         current = self.raw
-        if self.entities is None:
-            for i in range(self.dev):
+        if len(self.entities) == 0:
+            for i in range(self.dev+1):
                 current += randint(1, 10)
-            return int((current+self.bonus+bonus)*(1+(self.mult/10)))
+            return int((current+self.bonus+int(bonus))*(1+(self.mult/10)))
         else:
             for i in self.entities:
-                current += i.calcBudget(self.bonus+bonus)
+                current += i.calcBudget(self.bonus+int(bonus))
             return int(current*(1+(self.mult/10)))
 
-    def place(self, window, col, row):
-        self.name_e = Entry(window, width=15)
+    def place(self, col, row):
+
+        # Placing new Entries
+
+        self.name_e = Entry(self.window, width=15)
         self.name_e.delete(0, END)
         self.name_e.insert(0, self.name)
         self.name_e.grid(column=col, row=row)
 
-        self.dev_e = Entry(window, width=15)
+        self.dev_e = Entry(self.window, width=15)
         self.dev_e.delete(0, END)
         self.dev_e.insert(0, self.dev)
         self.dev_e.grid(column=col, row=row+1)
 
-        self.bonus_e = Entry(window, width=15)
+        self.bonus_e = Entry(self.window, width=15)
         self.bonus_e.delete(0, END)
         self.bonus_e.insert(0, self.dev)
         self.bonus_e.grid(column=col, row=row+2)
 
-        self.mult_e = Entry(window, width=15)
+        self.mult_e = Entry(self.window, width=15)
         self.mult_e.delete(0, END)
         self.mult_e.insert(0, self.mult)
         self.mult_e.grid(column=col, row=row+3)
 
-        self.raw_e = Entry(window, width=15)
+        self.raw_e = Entry(self.window, width=15)
         self.raw_e.delete(0, END)
         self.raw_e.insert(0, self.raw)
         self.raw_e.grid(column=col, row=row+4)
 
+        # Placing the button to add new entities
+
+        self.add_btn = Button(self.window, width=15, text="Add Entity", command=self.add)
+        self.add_btn.grid(column=col, row=row+5)
+
         temp = row
 
-        if self.entities is not None:
-            row+=4
-            for i in self.entities:
-                col+=1
-                (temp, col) = i.place(window, col, row+5)
+        self.remove_btn = []
+
+        # Remove entity buttons
+        for i in range(len(self.entities)):
+            self.remove_btn.append(Button(self.window, width=15, text="Remove Entity", command=partial(self.remove, i)))
+
+        row+=4
+        for i in range(len(self.entities)):
+            col+=1
+            self.remove_btn[i].grid(column=col, row=row)
+            (temp, col) = self.entities[i].place(col, row+1)
 
         return (temp,col)
 
-    def destroy(self):
+    def destroy_gui(self):
         self.name_e.destroy()
         self.dev_e.destroy()
         self.bonus_e.destroy()
         self.mult_e.destroy()
         self.raw_e.destroy()
-        if self.entities is not None:
-            for i in self.entities:
-                i.destroy()
+
+        self.add_btn.destroy()
+
+        for i in self.remove_btn:
+            i.destroy()
+
+        for i in self.entities:
+            i.destroy_gui()
 
     def update(self):
         self.name = self.name_e.get()
@@ -86,6 +132,23 @@ class entity:
         self.bonus = int(self.bonus_e.get())
         self.mult = int(self.mult_e.get())
         self.raw = int(self.raw_e.get())
-        if self.entities is not None:
-            for i in self.entities:
-                i.update()
+
+        for i in self.entities:
+            i.update()
+
+    def add(self):
+        self.entities.append(entity(self.window))
+        self.remove_btn.append(Button(self.window, width=15, text="Remove Entity", command=partial(self.remove, len(self.entities)-1)))
+
+    def remove(self, number):
+        for i in range(len(self.remove_btn)):
+            if i == number:
+                self.destroy_gui()
+                self.entities.pop(i)
+                self.remove_btn.pop(i)
+
+    def toString(self, ):
+        print("Name: %s, Dev: %i, Bonus: %i, Mult: %i, Raw: %i" %(self.name, self.dev, self.bonus, self.mult, self.raw))
+        print(self.entities)
+        for i in self.entities:
+            i.toString()
